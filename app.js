50db4c307165540de7b9cb250ae203c086700a5d
“JoO17pt“ <joao.pedro.c.araujo@gmail.com>
2022-08-16 16:44:36 +0100

    Layout basics

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+import languageOptions from "./data.js";
+
+// console.log(languageOptions);
+console.log("Teste");
+
 const button = document.querySelector('button');
 console.log(button);
+
+const languageSelect = document.querySelector('select')
+languageSelect.innerHTML += languageOptions;
 
 button.addEventListener('click', teste);
 
 function teste () {
     responsiveVoice.speak("Teste", "Brazilian Portuguese Female");
+    console.log("Fui clicado")
 }
+
+document.querySelector('h1').innerText = "Muda o texto"
